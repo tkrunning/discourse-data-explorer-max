55122ba6914587b8c17984484603d5d84dc34afe
Thomas K. Running <thomas@nomadgate.com>
2022-10-14 14:18:45 +0200

    remove site setting options

--- plugin.rb
+++ plugin.rb
@@ -1,4 +1,4 @@
-# name: discourse-data-explorer-max
+# name: x-discourse-data-explorer-max
 # about: Plugin to override the data explorer plugin max settings
 # version: 0.0.2
 # authors: Thomas K. Running
@@ -7,11 +7,6 @@
 enabled_site_setting :data_explorer_max_enabled
 
 module ::DataExplorer
-  if SiteSetting.data_explorer_max_query_result_default_limit
-    QUERY_RESULT_DEFAULT_LIMIT = SiteSetting.data_explorer_max_query_result_default_limit
-    QUERY_RESULT_MAX_LIMIT = SiteSetting.data_explorer_max_query_result_max_limit
-  else
-    QUERY_RESULT_DEFAULT_LIMIT = 1
-    QUERY_RESULT_MAX_LIMIT = 1000000
-  end
+  QUERY_RESULT_DEFAULT_LIMIT = 1
+  QUERY_RESULT_MAX_LIMIT = 1000000
 end
