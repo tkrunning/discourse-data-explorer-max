c489dd1e674dbdc4ae82224e8193022c317a5797
Thomas K. Running <thomas@nomadgate.com>
2022-10-14 15:12:19 +0200

    remove unused site settings

--- plugin.rb
+++ plugin.rb
@@ -2,7 +2,7 @@
 # about: Plugin to override the data explorer plugin max settings
 # version: 0.0.2
 # authors: Thomas K. Running
-# url: https://github.com/tkrunning/discourse-data-explorer-max
+# url: https://github.com/tkrunning/x-discourse-data-explorer-max
 
 enabled_site_setting :data_explorer_max_enabled
 
