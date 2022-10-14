4b69594d137d6ac6fbb9b7693ed2f5f1bea53e0c
Thomas K. Running <thomas@nomadgate.com>
2022-10-14 13:25:56 +0200

    add plugin settings, rm self def

--- plugin.rb
+++ plugin.rb
@@ -1,15 +1,13 @@
 # name: discourse-data-explorer-max
 # about: Plugin to override the data explorer plugin max settings
-# version: 0.0.1
+# version: 0.0.2
 # authors: Thomas K. Running
 # url: https://github.com/tkrunning/discourse-data-explorer-max
 
 module ::DataExplorer
-  QUERY_RESULT_DEFAULT_LIMIT = 10000
-  QUERY_RESULT_MAX_LIMIT = 100000
-
-          
-  def self.plugin_name
-    'discourse-data-explorer'.freeze
-  end
+  QUERY_RESULT_DEFAULT_LIMIT = 100000
+  QUERY_RESULT_MAX_LIMIT = 1000000
+  # QUERY_RESULT_DEFAULT_LIMIT = SiteSetting.data_explorer_max_query_result_default_limit
+  # QUERY_RESULT_MAX_LIMIT = SiteSetting.data_explorer_max_query_result_max_limit
+  
 end
